0ada3b1eb71f48a8366e39141fc110c0ccc0ffec
brenn <brennan.bertola@comcast.net>
2021-11-03 15:25:13 -0600

    initial set up

---
+++ src/views/Cart.vue
@@ -0,0 +1,65 @@
+<template>
+<div>
+  <h1>Alter component, make similar to ProductList.vue but removes instead, add message when cart is empty</h1>
+  <h1>Shopping Cart</h1>
+  <ProductList :products="products" />
+</div>
+</template>
+
+
+
+<script>
+import ProductList from "../components/ProductList.vue"
+export default {
+  name: 'Cart',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+      searchText: '',
+    }
+  },
+
+  computed: {
+    products() {
+      return this.$root.$data.cart;
+    }
+
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.search {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 50%;
+}
+
+form {
+  display: table;
+  width: 100%;
+}
+
+i {
+  display: table-cell;
+  padding-left: 10px;
+  width: 1px;
+}
+
+input {
+  display: table-cell;
+  font-size: 20px;
+  border: none !important;
+  box-shadow: none !important;
+  width: 100%;
+  height: 40px;
+}
+</style>
